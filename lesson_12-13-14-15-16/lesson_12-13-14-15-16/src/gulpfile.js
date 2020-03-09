07dfe6f0f68f41d3a62ca13861f398efee0c0882
Timur <trew.tr2019@yandex.ru>
2020-03-09 19:23:18 +0300

    add final folder

---
+++ lesson_12-13-14-15-16/lesson_12-13-14-15-16/src/gulpfile.js
@@ -0,0 +1,43 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const serveScss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Static server
+ function bs() {
+   serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.scss").on('change', browserSync.reload);
+  watch("./sass/**/*.sass").on('change', browserSync.reload);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+
+function minifycss(done) {
+  gulp.src('css/main.css')
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css/'));
+    done();
+};
+
+
+function serveSass() {
+  return src("./sass/**/*.sass", ".scss/**/*.scss")
+    .pipe(serveScss())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;
