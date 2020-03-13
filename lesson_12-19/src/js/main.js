33e06d03cd5a1c304f284ef26e7acb47ed85f90f
Timur <trew.tr2019@yandex.ru>
2020-03-14 02:14:13 +0300

    add close modal function's by 1) press esc; 2) press everywhere except the modal window

--- lesson_12-19/src/js/main.js
+++ lesson_12-19/src/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
+  const page = document.querySelector('.hero');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
@@ -12,4 +13,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   closeBtn.addEventListener('click', switchModal);
 
-});
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.classList.remove('modal--visible');
+    }
+  }
+
+  window.addEventListener('keydown', (event) => {
+    if (event.keyCode === 27) {
+      if (modal.classList.contains('modal--visible')) {
+        modal.classList.toggle('modal--visible');
+      }
+    }
+  })
+
+
+})
