031d9d944e1acb69b92ab4337910e92dd2048ff3
Timur <trew.tr2019@yandex.ru>
2020-03-16 02:35:23 +0300

    do second swiper

--- lesson_12-22/src/js/main.js
+++ lesson_12-22/src/js/main.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
 /*-------------------*/
 
 
-/* <-- Slider projects --> */
+/* <--    Slider's     --> */
     var mySwiper = new Swiper('.swiper-container', {
       loop: true,
       pagination: {
@@ -70,13 +70,51 @@ $(document).ready(function () {
       },
     })
 
+    var mySwiper2 = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: ('.steps__swiper-fraction'),
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: ('.steps__swiper-button-next'),
+        prevEl: ('.steps__swiper-button-prev'),
+      },
+    })  
+
+  $('.steps__swiper-button-next').on('click', function() {
+    var id = $('.item--active').attr('id')
+    $('.steps__swiper-menu__item').removeClass('item--active');
+    var activeId = parseInt(id) + 1;
+    if (activeId == 7) {
+      activeId = 1;
+    }
+    $('#' + activeId).addClass('item--active');
+  })
+
+  $('.steps__swiper-button-prev').on('click', function () {
+    var id = $('.item--active').attr('id')
+    $('.steps__swiper-menu__item').removeClass('item--active');
+    var activeId = parseInt(id) - 1;
+    if (activeId == 0) {
+      activeId = 6;
+    }
+    $('#' + activeId).addClass('item--active');
+  })
+
+  $('.steps__swiper-menu__item').on('click', function () {
+    var index = $(this).index()
+    mySwiper2[2].slideTo(index + 1);
+    $('.steps__swiper-menu__item').removeClass('item--active');
+    $(this).addClass('item--active');
+  });
 
     var next = $('.swiper-button-next');
     var prev = $('.swiper-button-prev');
     var bullets = $('.swiper-pagination');
 
     next.css('left', prev.width() + 20 + bullets.width() + 20);
-    bullets.css('left', prev.width() + 20)
+    bullets.css('left', prev.width() + 20);
 
 
 /*-------------------------*/
