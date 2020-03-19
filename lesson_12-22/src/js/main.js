edbb57c9892eb4257cef124e5dc49c7371bb11a6
Timur <trew.tr2019@yandex.ru>
2020-03-20 02:52:30 +0300

    add yandex map and do adaptive for it

--- lesson_12-22/src/js/main.js
+++ lesson_12-22/src/js/main.js
@@ -280,8 +280,45 @@ $(document).ready(function () {
 
    //mask for telephone
    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
-   $('.Username').mask('AAAAAAAAAAAAAAA');
 
-});
+
+
+
+   // Yandex Map
+     
+   ymaps.ready(function () {
+     var myMap = new ymaps.Map('map', {
+         center: [59.921269, 30.338426],
+         zoom: 9
+       }, {
+         searchControlProvider: 'yandex#search'
+       }),
+
+       // Создаём макет содержимого.
+       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+         '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+       ),
+
+       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+         hintContent: 'Наш оффис',
+         balloonContent: 'Вход со двора'
+       }, {
+         // Опции.
+         // Необходимо указать данный тип макета.
+         iconLayout: 'default#image',
+         // Своё изображение иконки метки.
+         iconImageHref: 'img/marker.png',
+         // Размеры метки.
+         iconImageSize: [32, 32],
+         // Смещение левого верхнего угла иконки относительно
+         // её "ножки" (точки привязки).
+         iconImageOffset: [-5, -38]
+       });
+
+     myMap.geoObjects
+       .add(myPlacemark);
+   });
+
+}); 
 
 
